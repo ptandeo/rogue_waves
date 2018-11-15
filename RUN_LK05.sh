a95068eb700b5cdc06c35fcb4cc79e7f3b466469
Pierre Tandeo <ptandeo@blanton.r-ccs27.riken.jp>
2018-11-15 18:28:12 +0900

    2018_11_15

--- RUN_LK05.sh
+++ RUN_LK05.sh
@@ -3,7 +3,7 @@
 echo "==parallel=="
 num_para=28 # Please set this below 28 at blanton!
 {
-   for i in $(seq 1 900); do
+   for i in $(seq 1 300); do
       echo $i
    done
 } | xargs -I{} -P${num_para} -n1 python GWP_randphi_LK05_PIERRE.py {}
